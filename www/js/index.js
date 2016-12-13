9c2c91dc8e244b8dd7e4071e70cfdec6efd605df
Wing Lun <wing@lunwing.com>
2016-12-13 20:31:41 +1000

    version4

--- www/js/index.js
+++ www/js/index.js
@@ -16,44 +16,44 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- 
+
 document.addEventListener("deviceready", onDeviceReady, false);
 
 
 function setupStatusBarGlow() {
-	var index = 0;
-	function setNextColor() {
-		index = index + 1;
-		if (index >= 512) {
-			index = 0;
-		}
-		var value = index;
-		if (value > 255) {
-			value = 512 - value;
-		}
-		var str = value.toString(16);
-		if (str.length == 1) {
-			str = '0' + str;
-		}
+  var index = 0;
+  function setNextColor() {
+    index = index + 1;
+    if (index >= 512) {
+      index = 0;
+    }
+    var value = index;
+    if (value > 255) {
+      value = 512 - value;
+    }
+    var str = value.toString(16);
+    if (str.length == 1) {
+      str = '0' + str;
+    }
 
-		// CHANGE THIS PART if you want a different color.
-		// Remember color is: RRGGBB
-		// This color will go from 000000 to 0000FF (black to blue)
-		// Switch it around if you want to use different colors
-		var colorString = '0000' + str;
-		StatusBar.backgroundColorByHexString(colorString);
-	}
-	setInterval(setNextColor, 20);
+    // CHANGE THIS PART if you want a different color.
+    // Remember color is: RRGGBB
+    // This color will go from 000000 to 0000FF (black to blue)
+    // Switch it around if you want to use different colors
+    var colorString = '0000' + str;
+    StatusBar.backgroundColorByHexString(colorString);
+  }
+  setInterval(setNextColor, 20);
 }
 
 function onDeviceReady() {
-    console.log(StatusBar);
-	StatusBar.show();
-	StatusBar.backgroundColorByHexString("1A468C");
-	// COMMENTED: don't actually want status bar glow
-	//setupStatusBarGlow();
+  console.log(StatusBar);
+  StatusBar.show();
+  StatusBar.backgroundColorByHexString("1A468C");
+  // COMMENTED: don't actually want status bar glow
+  //setupStatusBarGlow();
 };
-	
+
 
 /*function onload(){	
 	document.addEventListener("deviceready", onDeviceReady, false);
@@ -82,138 +82,138 @@ function playAudio(url) {
 
 
 
-  // These are the supported stations, mapped to the ID and URL
-  // of the stream
-  var STATIONS = {
-    'nac_radio': {
-      streamId: 'nundahac',
-      url: 'http://thassos.cdnstream.com:5049/live',
-      logo:'http://www.nac.org.au/wp-content/uploads/nac_easylistening.png'
-    },
-    'dcvs-radio': {
-      streamId: 'donrudd',
-      url: 'http://thassos.cdnstream.com:5046/stream',
-      logo:'http://www.nac.org.au/wp-content/uploads/dcvs_radio.png'
-    },
-  };
-
-
-  function play() {
-    var audio = document.getElementById("radio");
-    audio.load();
-    audio.play();
-  };
-
-  function pause(){
-    var audio = document.getElementById("radio");
-    audio.pause();
-
-  };
-
-  function defaultvolume(){
-    var audio = document.getElementById("radio");
-    audio.volume=0.7;
-  }
-  function decVol() {
-    var audio = document.getElementById("radio");
-    audio.volume -= 0.2;
-  };
-
-  function incVol(){
-    var audio = document.getElementById("radio");
-    audio.volume += 0.2;
-  };
-
-  // This function adds click handler on the station links.
-  // The purpose is to force the page to reload with a modified hash.
-  function setupLinks() {
-    $('#channels a').click(function(event){
-      var elem = $(event.currentTarget);
-      var baseUrl = window.location.href.split('#')[0];
-      var newUrl = baseUrl + elem.attr('href');
-      location.replace(newUrl);
-      location.reload();
-    });
-  }
-
-  // This function adjusts the audio and cc_streaminfo elements to
-  // match the selected station.  It must be called before
-  // the js from thassos is loaded.
-  function chooseStation() {
-    var stationId = window.location.hash.substr(1);
-
-    if (!stationId) {
-      // this is the default if none selected
-      stationId = 'nac_radio';
-    }
-
-    var station = STATIONS[stationId];
-    var streamId = station.streamId;
-    var url = station.url;
-    var logo = station.logo;
-
-    $('.cc_streaminfo').each(function(idx, elem) {
-      // add the selected streamId onto .cc_streaminfo element IDs,
-      // e.g. cc_strinfo_title => cc_strinfo_title_nundahac
-      elem = $(elem);
-      elem.attr('id', elem.attr('id') + '_' + streamId);
-    });
-    $('#radio').attr('src', url);
-    $('.radiologo img').attr('src',logo);
-  }
-
-function loadStreamInfo(){
-	console.log("About to load streaminfo.js");
-	var url = 'https://thassos.cdnstream.com:2199/system/streaminfo.js';
-	var scriptElement = document.createElement('script');
-	scriptElement.setAttribute('src', url);
-	document.getElementsByTagName('body')[0].appendChild(scriptElement);
-	console.log('streaminfo.js script has been appended');
-}
-
-  // Do all initialization prior to loading thassos JS.
-  function init() {
-    setupLinks();
-    chooseStation();
-    /* volume(); */
-	$(document).ready(loadStreamInfo);
-  }
-
-  init();
-
-
-  function play() {
-	var audio = document.getElementById("radio");
-    audio.load();
-    audio.play();
+// These are the supported stations, mapped to the ID and URL
+// of the stream
+var STATIONS = {
+  'nac_radio': {
+    streamId: 'nundahac',
+    url: 'http://thassos.cdnstream.com:5049/live',
+    logo: 'http://www.nac.org.au/wp-content/uploads/nac_easylistening.png'
+  },
+  'dcvs-radio': {
+    streamId: 'donrudd',
+    url: 'http://thassos.cdnstream.com:5046/stream',
+    logo: 'http://www.nac.org.au/wp-content/uploads/dcvs_radio.png'
+  },
 };
 
-function pause(){
-	var audio = document.getElementById("radio");
-	audio.pause();
+
+function play() {
+  var audio = document.getElementById("radio");
+  audio.load();
+  audio.play();
+};
+
+function pause() {
+  var audio = document.getElementById("radio");
+  audio.pause();
+
+};
+
+function defaultvolume() {
+  var audio = document.getElementById("radio");
+  audio.volume = 0.7;
+}
+function decVol() {
+  var audio = document.getElementById("radio");
+  audio.volume -= 0.2;
+};
+
+function incVol() {
+  var audio = document.getElementById("radio");
+  audio.volume += 0.2;
+};
+
+// This function adds click handler on the station links.
+// The purpose is to force the page to reload with a modified hash.
+function setupLinks() {
+  $('#channels a').click(function (event) {
+    var elem = $(event.currentTarget);
+    var baseUrl = window.location.href.split('#')[0];
+    var newUrl = baseUrl + elem.attr('href');
+    location.replace(newUrl);
+    location.reload();
+  });
+}
+
+// This function adjusts the audio and cc_streaminfo elements to
+// match the selected station.  It must be called before
+// the js from thassos is loaded.
+function chooseStation() {
+  var stationId = window.location.hash.substr(1);
+
+  if (!stationId) {
+    // this is the default if none selected
+    stationId = 'nac_radio';
+  }
+
+  var station = STATIONS[stationId];
+  var streamId = station.streamId;
+  var url = station.url;
+  var logo = station.logo;
+
+  $('.cc_streaminfo').each(function (idx, elem) {
+    // add the selected streamId onto .cc_streaminfo element IDs,
+    // e.g. cc_strinfo_title => cc_strinfo_title_nundahac
+    elem = $(elem);
+    elem.attr('id', elem.attr('id') + '_' + streamId);
+  });
+  $('#radio').attr('src', url);
+  $('.radiologo img').attr('src', logo);
+}
+
+function loadStreamInfo() {
+  console.log("About to load streaminfo.js");
+  var url = 'https://thassos.cdnstream.com:2199/system/streaminfo.js';
+  var scriptElement = document.createElement('script');
+  scriptElement.setAttribute('src', url);
+  document.getElementsByTagName('body')[0].appendChild(scriptElement);
+  console.log('streaminfo.js script has been appended');
+}
+
+// Do all initialization prior to loading thassos JS.
+function init() {
+  setupLinks();
+  chooseStation();
+  /* volume(); */
+  $(document).ready(loadStreamInfo);
+}
+
+init();
+
+
+function play() {
+  var audio = document.getElementById("radio");
+  audio.load();
+  audio.play();
+};
+
+function pause() {
+  var audio = document.getElementById("radio");
+  audio.pause();
 };
 
 
 function playPause() {
-    var audio = document.getElementById("radio");
-	var icon = document.getElementById('playicon');
-    if (audio.paused) {
-		audio.load();
-        audio.play();
-		icon.src='img/icon3.png'
-    } else {
-       audio.pause();
-	   icon.src='img/icon2.png'
-    }
+  var audio = document.getElementById("radio");
+  var icon = document.getElementById("playicon");
+  if (audio.paused) {
+    audio.load();
+    audio.play();
+    icon.className = "pause";
+  } else {
+    audio.pause();
+    icon.className = "play";
+  }
 };
 
-/* Set the width of the side navigation to 250px */
+/* Set the width of the side navigation to 100% */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "100%";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
 };
 
